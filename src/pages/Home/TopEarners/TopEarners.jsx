0d8a37b1eadf5b3cd8efd7dad5f64273e0584c41
Mojammel Noman <molla.noman23@gmail.com>
2024-06-22 21:56:43 +0600

    top earners data fetched and render

--- src/pages/Home/TopEarners/TopEarners.jsx
+++ src/pages/Home/TopEarners/TopEarners.jsx
@@ -1,6 +1,20 @@
-import TopEarnersCard from "./Card";
+import { useQuery } from "@tanstack/react-query";
+
+import useAxiosInstance from "../../../hooks/useAxiosInstance";
+import TopEarnersCard from "./TopEarnersCard";
+import SpinnerComponent from "../../../components/Spinner/Spinner";
 
 export default function TopEarners() {
+  const axiosInstance = useAxiosInstance();
+
+  const { data = [], isPending } = useQuery({
+    queryKey: ["topEarners"],
+    queryFn: async () => {
+      const res = await axiosInstance.get(`/topEarners`);
+      return res.data;
+    },
+  });
+
   return (
     <div className="my-24">
       <div className="text-3xl md:text-4xl text-black text-center mb-10">
@@ -12,10 +26,11 @@ export default function TopEarners() {
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 ">
-        <TopEarnersCard />
-        <TopEarnersCard />
-        <TopEarnersCard />
-        <TopEarnersCard />
+        {isPending && <SpinnerComponent />}
+
+        {data.map((user) => (
+          <TopEarnersCard key={user._id} user={user} />
+        ))}
       </div>
     </div>
   );
--- src/pages/Home/TopEarners/TopEarnersCard.jsx
+++ src/pages/Home/TopEarners/TopEarnersCard.jsx
@@ -1,22 +1,20 @@
 import { Card } from "flowbite-react";
 import manageWorkImg from "../../../assets/slider/manage-work.jpg";
 
-export default function TopEarnersCard() {
+export default function TopEarnersCard({ user }) {
   return (
     <Card className="max-w-sm bg-gray-100 border-blue-500">
       <div className="flex gap-5">
         <div>
-          <img src={manageWorkImg} alt="reviewer image" className="w-20 h-20" />
+          <img src={user.photoURL} alt="reviewer image" className="w-20 h-20" />
         </div>
 
         <div className="text-black">
-          <h5 className="text-2xl">Jack Grelish</h5>
+          <h5 className="text-2xl">{user.name}</h5>
 
           <p>
-            Total Coins: <span className="text-blue-500 font-bold">100</span>
-          </p>
-          <p>
-            Job Done: <span className="text-blue-500 font-bold">6</span>
+            Total Coins:{" "}
+            <span className="text-blue-500 font-bold">{user.coins}</span>
           </p>
         </div>
       </div>
